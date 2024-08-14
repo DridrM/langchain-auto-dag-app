d19652bf3747d87ae0856e223f5ab5aa00a8c23c
Hadrien Moncomble <hmoncomble@gmail.com>
2024-08-14 15:27:28 +0200

    Add command linne arguments to auto_dag.sh

--- auto_dag.sh
+++ auto_dag.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/bash
 
-# Read input scripts
-read -p "Enter python scripts paths : " -a scripts
+# Read input scripts as array
+scripts=("$@")
+
+# Verify if there is at least one input
+if [ ${#scripts[@]} -eq 0 ]; then
+  echo "Usage : $0 <path/to/script_1> ... <path/to/script_n>"
+  exit 1
+fi
 
 # Start the virtual environment
 poetry shell
