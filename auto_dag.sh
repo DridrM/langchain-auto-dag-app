665d59c39282f1460c2ec6fcfe49d81d4e449745
Hadrien Moncomble <hmoncomble@gmail.com>
2024-08-18 19:10:19 +0200

    Update the auto_dag.sh

--- auto_dag.sh
+++ auto_dag.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/zsh
 
+# Default name for the auto_dag cli command
+# If you change the default name here, pleas make sure
+# to do the same inside the install.sh file
+default_name="autodag"
+
 # Read input scripts as array
 scripts=("$@")
 
 # Verify if there is at least one input
 if [ ${#scripts[@]} -eq 0 ]; then
-  echo "Usage : $0 <path/to/script_1> ... <path/to/script_n>"
+  echo "Usage : $default_name <path/to/script_1> ... <path/to/script_n>"
   exit 1
 fi
 
